13c4af6a4a32439faa8e36243753d82bd543110b
Ibtasam Haider Riddho <ihriddho07@gmail.com>
2026-08-10 00:31:27 +0600

    Set up basic game window and draw elements
    Initialize a simple Frogger game window with a river and road.

--- codes/frogger_riddho.c
+++ codes/frogger_riddho.c
@@ -1,1 +1,40 @@
+#include "raylib.h"
 
+#define HEIGHT 1000
+#define WIDTH 1200
+
+int main(void)
+{   float i_cap=WIDTH/15.0f;
+    float j_cap=HEIGHT/15.0f;
+
+    Vector2 river ={0.0f,j_cap*2};
+    Vector2 road={0.0f,j_cap*8};
+
+    InitWindow(WIDTH,HEIGHT,"FROGGER");
+    SetTargetFPS(60);
+
+
+    while(!WindowShouldClose())
+{
+
+BeginDrawing();
+ClearBackground((Color){0,0,0,255});
+DrawRectangle(river.x,river.y,WIDTH,j_cap*5,(Color){0, 43, 77,255});
+DrawRectangle(road.x,road.y,WIDTH,j_cap*5,(Color){0,0,0,255});
+
+
+
+
+
+EndDrawing();
+
+
+
+}
+
+
+
+
+    CloseWindow();
+    return 0;
+}
